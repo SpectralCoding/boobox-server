4f04241dea791de19a7847b1bb3527b4be7f3f3d
Caesar Kabalan <Dandelock@gmail.com>
2010-04-22 04:58:22 +0000

    Server: MainFrm UI redesign. No functional code as of yet.
    git-svn-id: file:///content/svn/csharp/boobox/BooBoxServer@14 df8029df-fac5-43eb-a43f-3a0073f54a4e

--- MainFrm.Designer.cs
+++ MainFrm.Designer.cs
@@ -25,10 +25,6 @@
 		private void InitializeComponent() {
 			this.MenuStrip = new System.Windows.Forms.MenuStrip();
 			this.FileMenuHeader = new System.Windows.Forms.ToolStripMenuItem();
-			this.HelpMenuHeader = new System.Windows.Forms.ToolStripMenuItem();
-			this.HelpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
-			this.AboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.SaveSettingsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
 			this.ExportSettingsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,11 +32,36 @@
 			this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
 			this.ExitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.OptionsHeaderMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.HelpMenuHeader = new System.Windows.Forms.ToolStripMenuItem();
+			this.HelpMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.AboutMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.StatusStrip = new System.Windows.Forms.StatusStrip();
 			this.ProgressBarStatusStrip = new System.Windows.Forms.ToolStripProgressBar();
 			this.ProgressBarLblStatusStrip = new System.Windows.Forms.ToolStripStatusLabel();
+			this.LibraryGrp = new System.Windows.Forms.GroupBox();
+			this.PlaylistGrp = new System.Windows.Forms.GroupBox();
+			this.LibraryDGV = new System.Windows.Forms.DataGridView();
+			this.PlaylistDGV = new System.Windows.Forms.DataGridView();
+			this.PlaylistComb = new System.Windows.Forms.ComboBox();
+			this.AddToPlaylistCmd = new System.Windows.Forms.Button();
+			this.DelCmd = new System.Windows.Forms.Button();
+			this.ToBottomCmd = new System.Windows.Forms.Button();
+			this.DownCmd = new System.Windows.Forms.Button();
+			this.UpCmd = new System.Windows.Forms.Button();
+			this.ToTopCmd = new System.Windows.Forms.Button();
+			this.DeletePlaylistCmd = new System.Windows.Forms.Button();
+			this.NewPlaylistCmd = new System.Windows.Forms.Button();
+			this.AddFolderToLibraryMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.RemoveFolderFromLibraryMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.RebuildLibraryMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripSeparator();
 			this.MenuStrip.SuspendLayout();
 			this.StatusStrip.SuspendLayout();
+			this.LibraryGrp.SuspendLayout();
+			this.PlaylistGrp.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.LibraryDGV)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.PlaylistDGV)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// MenuStrip
@@ -51,7 +72,7 @@
             this.HelpMenuHeader});
 			this.MenuStrip.Location = new System.Drawing.Point(0, 0);
 			this.MenuStrip.Name = "MenuStrip";
-			this.MenuStrip.Size = new System.Drawing.Size(706, 24);
+			this.MenuStrip.Size = new System.Drawing.Size(711, 24);
 			this.MenuStrip.TabIndex = 0;
 			// 
 			// FileMenuHeader
@@ -59,6 +80,10 @@
 			this.FileMenuHeader.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.SaveSettingsMenuItem,
             this.toolStripMenuItem2,
+            this.AddFolderToLibraryMenuItem,
+            this.RemoveFolderFromLibraryMenuItem,
+            this.RebuildLibraryMenuItem,
+            this.toolStripMenuItem4,
             this.ExportSettingsMenuItem,
             this.ImportSettingsMenuItem,
             this.toolStripMenuItem3,
@@ -66,6 +91,48 @@
 			this.FileMenuHeader.Name = "FileMenuHeader";
 			this.FileMenuHeader.Size = new System.Drawing.Size(37, 20);
 			this.FileMenuHeader.Text = "&File";
+			// 
+			// SaveSettingsMenuItem
+			// 
+			this.SaveSettingsMenuItem.Name = "SaveSettingsMenuItem";
+			this.SaveSettingsMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.SaveSettingsMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.SaveSettingsMenuItem.Text = "&Save Settings...";
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(224, 6);
+			// 
+			// ExportSettingsMenuItem
+			// 
+			this.ExportSettingsMenuItem.Name = "ExportSettingsMenuItem";
+			this.ExportSettingsMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.ExportSettingsMenuItem.Text = "&Export Settings...";
+			// 
+			// ImportSettingsMenuItem
+			// 
+			this.ImportSettingsMenuItem.Name = "ImportSettingsMenuItem";
+			this.ImportSettingsMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.ImportSettingsMenuItem.Text = "&Import Settings...";
+			// 
+			// toolStripMenuItem3
+			// 
+			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+			this.toolStripMenuItem3.Size = new System.Drawing.Size(224, 6);
+			// 
+			// ExitMenuItem
+			// 
+			this.ExitMenuItem.Name = "ExitMenuItem";
+			this.ExitMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+			this.ExitMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.ExitMenuItem.Text = "E&xit";
+			// 
+			// OptionsHeaderMenu
+			// 
+			this.OptionsHeaderMenu.Name = "OptionsHeaderMenu";
+			this.OptionsHeaderMenu.Size = new System.Drawing.Size(61, 20);
+			this.OptionsHeaderMenu.Text = "&Options";
 			// 
 			// HelpMenuHeader
 			// 
@@ -96,54 +163,14 @@
 			this.AboutMenuItem.Text = "&About";
 			this.AboutMenuItem.Click += new System.EventHandler(this.AboutMenuItem_Click);
 			// 
-			// SaveSettingsMenuItem
-			// 
-			this.SaveSettingsMenuItem.Name = "SaveSettingsMenuItem";
-			this.SaveSettingsMenuItem.Size = new System.Drawing.Size(164, 22);
-			this.SaveSettingsMenuItem.Text = "&Save Settings...";
-			// 
-			// toolStripMenuItem2
-			// 
-			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-			this.toolStripMenuItem2.Size = new System.Drawing.Size(161, 6);
-			// 
-			// ExportSettingsMenuItem
-			// 
-			this.ExportSettingsMenuItem.Name = "ExportSettingsMenuItem";
-			this.ExportSettingsMenuItem.Size = new System.Drawing.Size(164, 22);
-			this.ExportSettingsMenuItem.Text = "&Export Settings...";
-			// 
-			// ImportSettingsMenuItem
-			// 
-			this.ImportSettingsMenuItem.Name = "ImportSettingsMenuItem";
-			this.ImportSettingsMenuItem.Size = new System.Drawing.Size(164, 22);
-			this.ImportSettingsMenuItem.Text = "&Import Settings...";
-			// 
-			// toolStripMenuItem3
-			// 
-			this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-			this.toolStripMenuItem3.Size = new System.Drawing.Size(161, 6);
-			// 
-			// ExitMenuItem
-			// 
-			this.ExitMenuItem.Name = "ExitMenuItem";
-			this.ExitMenuItem.Size = new System.Drawing.Size(164, 22);
-			this.ExitMenuItem.Text = "E&xit";
-			// 
-			// OptionsHeaderMenu
-			// 
-			this.OptionsHeaderMenu.Name = "OptionsHeaderMenu";
-			this.OptionsHeaderMenu.Size = new System.Drawing.Size(61, 20);
-			this.OptionsHeaderMenu.Text = "&Options";
-			// 
 			// StatusStrip
 			// 
 			this.StatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ProgressBarStatusStrip,
             this.ProgressBarLblStatusStrip});
-			this.StatusStrip.Location = new System.Drawing.Point(0, 299);
+			this.StatusStrip.Location = new System.Drawing.Point(0, 585);
 			this.StatusStrip.Name = "StatusStrip";
-			this.StatusStrip.Size = new System.Drawing.Size(706, 22);
+			this.StatusStrip.Size = new System.Drawing.Size(711, 22);
 			this.StatusStrip.TabIndex = 1;
 			this.StatusStrip.Text = "statusStrip1";
 			// 
@@ -159,20 +186,179 @@
 			this.ProgressBarLblStatusStrip.Size = new System.Drawing.Size(39, 17);
 			this.ProgressBarLblStatusStrip.Text = "Ready";
 			// 
+			// LibraryGrp
+			// 
+			this.LibraryGrp.Controls.Add(this.LibraryDGV);
+			this.LibraryGrp.Location = new System.Drawing.Point(6, 27);
+			this.LibraryGrp.Name = "LibraryGrp";
+			this.LibraryGrp.Size = new System.Drawing.Size(699, 253);
+			this.LibraryGrp.TabIndex = 2;
+			this.LibraryGrp.TabStop = false;
+			this.LibraryGrp.Text = "Music Library";
+			// 
+			// PlaylistGrp
+			// 
+			this.PlaylistGrp.Controls.Add(this.DelCmd);
+			this.PlaylistGrp.Controls.Add(this.ToBottomCmd);
+			this.PlaylistGrp.Controls.Add(this.DownCmd);
+			this.PlaylistGrp.Controls.Add(this.UpCmd);
+			this.PlaylistGrp.Controls.Add(this.ToTopCmd);
+			this.PlaylistGrp.Controls.Add(this.PlaylistDGV);
+			this.PlaylistGrp.Location = new System.Drawing.Point(6, 313);
+			this.PlaylistGrp.Name = "PlaylistGrp";
+			this.PlaylistGrp.Size = new System.Drawing.Size(699, 267);
+			this.PlaylistGrp.TabIndex = 3;
+			this.PlaylistGrp.TabStop = false;
+			this.PlaylistGrp.Text = "Playlist";
+			// 
+			// LibraryDGV
+			// 
+			this.LibraryDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.LibraryDGV.Location = new System.Drawing.Point(6, 19);
+			this.LibraryDGV.Name = "LibraryDGV";
+			this.LibraryDGV.Size = new System.Drawing.Size(687, 227);
+			this.LibraryDGV.TabIndex = 0;
+			// 
+			// PlaylistDGV
+			// 
+			this.PlaylistDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.PlaylistDGV.Location = new System.Drawing.Point(6, 19);
+			this.PlaylistDGV.Name = "PlaylistDGV";
+			this.PlaylistDGV.Size = new System.Drawing.Size(648, 241);
+			this.PlaylistDGV.TabIndex = 1;
+			// 
+			// PlaylistComb
+			// 
+			this.PlaylistComb.FormattingEnabled = true;
+			this.PlaylistComb.Location = new System.Drawing.Point(12, 286);
+			this.PlaylistComb.Name = "PlaylistComb";
+			this.PlaylistComb.Size = new System.Drawing.Size(420, 21);
+			this.PlaylistComb.TabIndex = 4;
+			// 
+			// AddToPlaylistCmd
+			// 
+			this.AddToPlaylistCmd.Location = new System.Drawing.Point(438, 284);
+			this.AddToPlaylistCmd.Name = "AddToPlaylistCmd";
+			this.AddToPlaylistCmd.Size = new System.Drawing.Size(85, 23);
+			this.AddToPlaylistCmd.TabIndex = 5;
+			this.AddToPlaylistCmd.Text = "Add To Playlist";
+			this.AddToPlaylistCmd.UseVisualStyleBackColor = true;
+			// 
+			// DelCmd
+			// 
+			this.DelCmd.Location = new System.Drawing.Point(660, 123);
+			this.DelCmd.Name = "DelCmd";
+			this.DelCmd.Size = new System.Drawing.Size(33, 33);
+			this.DelCmd.TabIndex = 12;
+			this.DelCmd.Text = "Del";
+			this.DelCmd.UseVisualStyleBackColor = true;
+			// 
+			// ToBottomCmd
+			// 
+			this.ToBottomCmd.Location = new System.Drawing.Point(660, 214);
+			this.ToBottomCmd.Name = "ToBottomCmd";
+			this.ToBottomCmd.Size = new System.Drawing.Size(33, 46);
+			this.ToBottomCmd.TabIndex = 14;
+			this.ToBottomCmd.Text = "\\/ \\/";
+			this.ToBottomCmd.UseVisualStyleBackColor = true;
+			// 
+			// DownCmd
+			// 
+			this.DownCmd.Location = new System.Drawing.Point(660, 162);
+			this.DownCmd.Name = "DownCmd";
+			this.DownCmd.Size = new System.Drawing.Size(33, 46);
+			this.DownCmd.TabIndex = 13;
+			this.DownCmd.Text = "\\/";
+			this.DownCmd.UseVisualStyleBackColor = true;
+			// 
+			// UpCmd
+			// 
+			this.UpCmd.Location = new System.Drawing.Point(660, 71);
+			this.UpCmd.Name = "UpCmd";
+			this.UpCmd.Size = new System.Drawing.Size(33, 46);
+			this.UpCmd.TabIndex = 11;
+			this.UpCmd.Text = "/\\";
+			this.UpCmd.UseVisualStyleBackColor = true;
+			// 
+			// ToTopCmd
+			// 
+			this.ToTopCmd.Location = new System.Drawing.Point(660, 19);
+			this.ToTopCmd.Name = "ToTopCmd";
+			this.ToTopCmd.Size = new System.Drawing.Size(33, 46);
+			this.ToTopCmd.TabIndex = 10;
+			this.ToTopCmd.Text = "/\\ /\\";
+			this.ToTopCmd.UseVisualStyleBackColor = true;
+			// 
+			// DeletePlaylistCmd
+			// 
+			this.DeletePlaylistCmd.Location = new System.Drawing.Point(529, 284);
+			this.DeletePlaylistCmd.Name = "DeletePlaylistCmd";
+			this.DeletePlaylistCmd.Size = new System.Drawing.Size(85, 23);
+			this.DeletePlaylistCmd.TabIndex = 6;
+			this.DeletePlaylistCmd.Text = "Delete Playlist";
+			this.DeletePlaylistCmd.UseVisualStyleBackColor = true;
+			// 
+			// NewPlaylistCmd
+			// 
+			this.NewPlaylistCmd.Location = new System.Drawing.Point(620, 284);
+			this.NewPlaylistCmd.Name = "NewPlaylistCmd";
+			this.NewPlaylistCmd.Size = new System.Drawing.Size(85, 23);
+			this.NewPlaylistCmd.TabIndex = 7;
+			this.NewPlaylistCmd.Text = "New Playlist";
+			this.NewPlaylistCmd.UseVisualStyleBackColor = true;
+			// 
+			// AddFolderToLibraryMenuItem
+			// 
+			this.AddFolderToLibraryMenuItem.Name = "AddFolderToLibraryMenuItem";
+			this.AddFolderToLibraryMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+			this.AddFolderToLibraryMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.AddFolderToLibraryMenuItem.Text = "&Add Folder to Library";
+			// 
+			// RemoveFolderFromLibraryMenuItem
+			// 
+			this.RemoveFolderFromLibraryMenuItem.Name = "RemoveFolderFromLibraryMenuItem";
+			this.RemoveFolderFromLibraryMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.RemoveFolderFromLibraryMenuItem.Text = "&Remove Folder from Library";
+			// 
+			// RebuildLibraryMenuItem
+			// 
+			this.RebuildLibraryMenuItem.Name = "RebuildLibraryMenuItem";
+			this.RebuildLibraryMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+			this.RebuildLibraryMenuItem.Size = new System.Drawing.Size(227, 22);
+			this.RebuildLibraryMenuItem.Text = "Re&build Library";
+			// 
+			// toolStripMenuItem4
+			// 
+			this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+			this.toolStripMenuItem4.Size = new System.Drawing.Size(224, 6);
+			// 
 			// MainFrm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(706, 321);
+			this.ClientSize = new System.Drawing.Size(711, 607);
+			this.Controls.Add(this.NewPlaylistCmd);
+			this.Controls.Add(this.DeletePlaylistCmd);
+			this.Controls.Add(this.AddToPlaylistCmd);
+			this.Controls.Add(this.PlaylistComb);
+			this.Controls.Add(this.PlaylistGrp);
+			this.Controls.Add(this.LibraryGrp);
 			this.Controls.Add(this.StatusStrip);
 			this.Controls.Add(this.MenuStrip);
 			this.MainMenuStrip = this.MenuStrip;
+			this.MinimumSize = new System.Drawing.Size(500, 645);
 			this.Name = "MainFrm";
 			this.Text = "BooBox Server : Name";
+			this.Load += new System.EventHandler(this.MainFrm_Load);
+			this.Resize += new System.EventHandler(this.MainFrm_Resize);
 			this.MenuStrip.ResumeLayout(false);
 			this.MenuStrip.PerformLayout();
 			this.StatusStrip.ResumeLayout(false);
 			this.StatusStrip.PerformLayout();
+			this.LibraryGrp.ResumeLayout(false);
+			this.PlaylistGrp.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.LibraryDGV)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.PlaylistDGV)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -196,6 +382,23 @@
 		private System.Windows.Forms.StatusStrip StatusStrip;
 		private System.Windows.Forms.ToolStripProgressBar ProgressBarStatusStrip;
 		private System.Windows.Forms.ToolStripStatusLabel ProgressBarLblStatusStrip;
+		private System.Windows.Forms.GroupBox LibraryGrp;
+		private System.Windows.Forms.GroupBox PlaylistGrp;
+		private System.Windows.Forms.DataGridView LibraryDGV;
+		private System.Windows.Forms.DataGridView PlaylistDGV;
+		private System.Windows.Forms.ComboBox PlaylistComb;
+		private System.Windows.Forms.Button AddToPlaylistCmd;
+		private System.Windows.Forms.Button DelCmd;
+		private System.Windows.Forms.Button ToBottomCmd;
+		private System.Windows.Forms.Button DownCmd;
+		private System.Windows.Forms.Button UpCmd;
+		private System.Windows.Forms.Button ToTopCmd;
+		private System.Windows.Forms.Button DeletePlaylistCmd;
+		private System.Windows.Forms.Button NewPlaylistCmd;
+		private System.Windows.Forms.ToolStripMenuItem AddFolderToLibraryMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem RemoveFolderFromLibraryMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem RebuildLibraryMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem4;
 	}
 }
 
--- MainFrm.cs
+++ MainFrm.cs
@@ -17,5 +17,51 @@ namespace BooBoxServer {
 			AboutFrm AboutFrm = new AboutFrm();
 			AboutFrm.Show();
 		}
+
+		private void MainFrm_Load(object sender, EventArgs e) {
+
+		}
+
+		private void MainFrm_Resize(object sender, EventArgs e) {
+			#region Horizontal Calculations
+			LibraryGrp.Width = this.Width - 28;
+			LibraryDGV.Width = LibraryGrp.Width - 12;
+			PlaylistComb.Width = this.Width - 307;
+			AddToPlaylistCmd.Left = this.Width - 289;
+			DeletePlaylistCmd.Left = this.Width - 198;
+			NewPlaylistCmd.Left = this.Width - 107;
+			PlaylistGrp.Width = this.Width - 28;
+			PlaylistDGV.Width = PlaylistGrp.Width - 51;
+			ToTopCmd.Left = PlaylistGrp.Width - 39;
+			UpCmd.Left = PlaylistGrp.Width - 39;
+			DelCmd.Left = PlaylistGrp.Width - 39;
+			DownCmd.Left = PlaylistGrp.Width - 39;
+			ToBottomCmd.Left = PlaylistGrp.Width - 39;
+			#endregion
+			#region Vertical Calculations
+			// Exact Calculation: 0.3922480620155039
+			LibraryGrp.Height = Convert.ToInt32(this.Height * 0.3923);
+			LibraryDGV.Height = LibraryGrp.Height - 26;
+			PlaylistComb.Top = LibraryGrp.Top + LibraryGrp.Height + 6;
+			AddToPlaylistCmd.Top = PlaylistComb.Top - 2;
+			DeletePlaylistCmd.Top = PlaylistComb.Top - 2;
+			NewPlaylistCmd.Top = PlaylistComb.Top - 2;
+			PlaylistGrp.Top = PlaylistComb.Top + 27;
+			PlaylistGrp.Height = this.Height - PlaylistGrp.Top - 65;
+			PlaylistDGV.Height = PlaylistGrp.Height - 26;
+			// Exact Calculation: 0.2119815668202765
+			int PlaylistButtonHeight = Convert.ToInt32((PlaylistDGV.Height - 23) * 0.212);
+			ToTopCmd.Height = PlaylistButtonHeight;
+			UpCmd.Height = PlaylistButtonHeight;
+			// Exact Calculation: 0.152073732718894
+			DelCmd.Height = Convert.ToInt32((PlaylistDGV.Height - 23) * 0.1521);
+			DownCmd.Height = PlaylistButtonHeight;
+			ToBottomCmd.Height = PlaylistButtonHeight;
+			UpCmd.Top = ToTopCmd.Top + ToTopCmd.Height + 6;
+			DelCmd.Top = UpCmd.Top + UpCmd.Height + 6;
+			DownCmd.Top = DelCmd.Top + DelCmd.Height + 6;
+			ToBottomCmd.Top = DownCmd.Top + DownCmd.Height + 6;
+			#endregion
+		}
 	}
 }
