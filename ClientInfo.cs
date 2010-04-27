35bedb1b3bee51161cda2006c5e432958c7c6784
Caesar Kabalan <Dandelock@gmail.com>
2010-04-27 09:19:11 +0000

    Client, Server & Lib: Added various communication functionality.
    git-svn-id: file:///content/svn/csharp/boobox/BooBoxServer@58 df8029df-fac5-43eb-a43f-3a0073f54a4e

--- ClientInfo.cs
+++ ClientInfo.cs
@@ -32,7 +32,7 @@ namespace BooBoxServer {
 		/// Triggered when the Client and Server have achieved a full TCP connection
 		/// </summary>
 		private void OnClientConnect() {
-
+			Send(Protocol.CreateHELLO(Config.Instance.ServerName));
 		}
 
 		/// <summary>
@@ -60,7 +60,68 @@ namespace BooBoxServer {
 			} else {
 				Console.WriteLine("From ClientInfo " + Index + ":\t" + Data);
 			}
-			//Protocol.ParseMessage(Data, this);
+			ParseMessage(Data);
+		}
+
+		public void ParseMessage(String Data) {
+			char[] spaceDelim = new char[] { ' ' };
+			String[] tokenData = Data.Split(spaceDelim, 2);
+			switch (tokenData[0]) {
+				case "HELLOR":
+					#region HELLOR
+					Name = tokenData[1];
+					Log.AddClientText("Connected to \"" + Name + "\".", Index);
+					Send(Protocol.CreateVERSION());
+					break;
+					#endregion
+				case "VERSIONR":
+					#region VERSIONR
+					Version = tokenData[1];
+					Log.AddClientText("\"" + Name + "\" running at version " + Version, Index);
+					Send(Protocol.CreateDETAILS(GenerateDetailList()));
+					if (Config.Instance.PasswordRequired == false) {
+						ConnectionStatus = ConnectionStatus.Connected;
+						Send(Protocol.CreateOK());
+					}
+					break;
+					#endregion
+				case "PASS":
+					#region PASS
+					if (Config.Instance.PasswordRequired == true) {
+						if (tokenData[1] == Config.Instance.ServerPassword) {
+							Log.AddClientText("Accepted password from \"" + Name + "\"", Index);
+							Send("PASSR PASS_ACCEPTED");
+							ConnectionStatus = ConnectionStatus.Connected;
+							Send(Protocol.CreateOK());
+							Log.AddClientText("Fully connected to \"" + Name + "\"!", Index);
+						} else {
+							Send("PASSR PASS_REJECTED");
+							Log.AddClientText("Rejected password from \"" + Name + "\"", Index);
+						}
+					} else {
+						Send("PASSR PASS_NOT_REQUIRED");
+						Log.AddClientText("\"" + Name + "\" sent password, but it was not required. Ingoring.", Index);
+					}
+					break;
+					#endregion
+				case "GOODBYE":
+					#region GOODBYE
+					Log.AddClientText("\"" + Name + "\" sent GOODBYE. Disconnecting...", Index);
+					Socket.Close();
+					break;
+					#endregion
+			}
+		}
+
+		private List<ServerDetail> GenerateDetailList() {
+			List<ServerDetail> tempDetailList = new List<ServerDetail>();
+			ServerDetail tempServerDetail = new ServerDetail();
+			tempServerDetail.Name = "GUID"; tempServerDetail.Value = Config.Instance.GUID; tempDetailList.Add(tempServerDetail);
+			tempServerDetail.Name = "STREAMPORT"; tempServerDetail.Value = Config.Instance.CommStreamPort.ToString(); tempDetailList.Add(tempServerDetail);
+			tempServerDetail.Name = "PASS";
+			if (Config.Instance.PasswordRequired == true) { tempServerDetail.Value = "1"; } else { tempServerDetail.Value = "0"; }
+			tempDetailList.Add(tempServerDetail);
+			return tempDetailList;
 		}
 
 
